6169484a32dfb10403761c534991641040eee41b
ibanezz <ibanezz@126.com>
2013-05-03 02:20:38 +0800

    Test

---
+++ WindowDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WindowDemo
+//
+//  Created by YanG on 13-1-20.
+//  Copyright (c) 2013年 YanG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WindowDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WindowDemo
+//
+//  Created by YanG on 13-1-20.
+//  Copyright (c) 2013年 YanG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
